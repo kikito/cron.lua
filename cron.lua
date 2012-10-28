4ea9f257a86e8d92a3ced5cd56c6773c727b5931
kikito <kikito@gmail.com>
2012-10-29 00:45:08 +0100

    bumped version to 1.2

--- cron.lua
+++ cron.lua
@@ -1,5 +1,5 @@
 -----------------------------------------------------------------------------------------------------------------------
--- cron.lua - v1.1 (2011-04)
+-- cron.lua - v1.2 (2012-10)
 -- Enrique García Cota - enrique.garcia.cota [AT] gmail [DOT] com
 -- time-related functions for Lua.
 -- inspired by Javascript's setTimeout and setInterval
