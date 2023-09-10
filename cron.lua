534298daeb70f5c3ceccb91d2a16af6dd2d8be32
David <skaarj1989@gmail.com>
2023-09-10 19:24:11 +0200

    Fix deprecated function call

--- cron.lua
+++ cron.lua
@@ -68,7 +68,7 @@ local function updateAfterClock(self, dt) -- returns true if expired
   self.running = self.running + dt
 
   if self.running >= self.time then
-    self.callback(unpack(self.args))
+    self.callback(table.unpack(self.args))
     return true
   end
   return false
@@ -80,7 +80,7 @@ local function updateEveryClock(self, dt)
   self.running = self.running + dt
 
   while self.running >= self.time do
-    self.callback(unpack(self.args))
+    self.callback(table.unpack(self.args))
     self.running = self.running - self.time
   end
   return false
